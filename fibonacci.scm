79f505f3c7fe26d99815400f222c1fb45d8864bc
[#571] <31502293+Number571@users.noreply.github.com>
2018-11-01 03:11:48 +0700

    Update fibonacci.scm

--- fibonacci.scm
+++ fibonacci.scm
@@ -1,14 +1,14 @@
 ; 2.1
 ;(define (fibonacci x)
-;	(cond ((= x 0) 0) 
-;		  ((= x 1) 1) 
-;		  (else (+ (fibonacci (- x 1))
-;				   (fibonacci (- x 2))))))
+;   (cond ((= x 0) 0) 
+;         ((= x 1) 1) 
+;         (else (+ (fibonacci (- x 1))
+;                  (fibonacci (- x 2))))))
 
 ; 2.2
 (define (fibonacci x)
-	(define (fib-iter n a b)
-		(if (= n 0)
-			b
-			(fib-iter (- n 1) (+ a b) a)))
-	(fib-iter x 1 0))
+    (define (fib-iter n a b)
+        (if (= n 0)
+            b
+            (fib-iter (- n 1) (+ a b) a)))
+    (fib-iter x 1 0))
