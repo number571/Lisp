5510fcfb711d242e7d475d195b647711534f93e9
[#571] <31502293+Number571@users.noreply.github.com>
2018-11-01 03:12:25 +0700

    Update factorial.scm

--- factorial.scm
+++ factorial.scm
@@ -1,13 +1,13 @@
-; 1.1
+; 1
 ;(define (factorial x)
-;	(if (< x 2)
-;		1
-;		(* x (factorial (- x 1)))))
+;   (if (< x 2)
+;       1
+;       (* x (factorial (- x 1)))))
 
-; 1.2
+; 2
 (define (factorial x) 
-	(define (fact-iter a b)
-		(if (< a 2)
-			b
-			(fact-iter (- a 1) (* b a))))
-	(fact-iter x 1))
+    (define (fact-iter a b)
+        (if (< a 2)
+            b
+            (fact-iter (- a 1) (* b a))))
+    (fact-iter x 1))
