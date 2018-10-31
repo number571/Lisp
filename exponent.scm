1d6ce28249a631c6ca231da15ab15b9c1f7019d9
[#571] <31502293+Number571@users.noreply.github.com>
2018-11-01 03:13:51 +0700

    Update exponent.scm

--- exponent.scm
+++ exponent.scm
@@ -1,20 +1,21 @@
-; 3.1
+; 1
 ;(define (exponent x y)
-;	(if (= y 0)
-;		1
-;		(* x (exponent x (- y 1)))))
+;   (if (= y 0)
+;       1
+;       (* x (exponent x (- y 1)))))
 
-; 3.2
+; 2
 ;(define (exponent x y)
-;	(define (exp-iter a b n)
-;		(if (= b 0) 
-;			n
-;			(exp-iter a (- b 1) (* n a))))
-;	(exp-iter x y 1))
+;   (define (exp-iter a b n)
+;       (if (= b 0) 
+;           n
+;           (exp-iter a (- b 1) (* n a))))
+;   (exp-iter x y 1))
 
-; 3.3
+; 3
+(define (square x) (* x x))
+
 (define (exponent x y)
-	(define (square x) (* x x))
-	(cond ((= y 0) 1)
-		  ((even? y) (square (exponent x (/ y 2))))
-		  (else (* x (exponent x (- y 1))))))
+    (cond ((= y 0) 1)
+          ((even? y) (square (exponent x (/ y 2))))
+          (else (* x (exponent x (- y 1))))))
