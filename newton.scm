35c99e896a473c8b58176689481689988889d967
[#571] <31502293+Number571@users.noreply.github.com>
2018-11-02 01:40:20 +0700

    Create newton.scm

---
+++ newton.scm
@@ -0,0 +1,21 @@
+(define (fixed-point f first-guess)
+    (define (close-enough? x y)
+        (< (abs (- x y)) 0.00001))
+    (define (try guess)
+        (let ((next (f guess)))
+            (if (close-enough? guess next)
+                next
+                (try next))))
+    (try first-guess))
+
+(define (deriv g)
+    (lambda (x)
+        (/ (- (g (+ x dx)) (g x))
+            dx)))
+
+(define (newton-transform g)
+    (lambda (x)
+        (- x (/ (g x) ((deriv g) x)))))
+
+(define (newton-method g guess)
+    (fixed-point (newton-transform g) guess))
